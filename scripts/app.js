0cbf48598c9aa0f3eba27456a9728490984d1c33
kenzodiac <kennethfujimura@gmail.com>
2023-01-24 13:32:28 -0800

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+//import {} from "./localStorage.js"
+
+let searchInput = document.getElementById("searchInput");
+let submitBtn = document.getElementById("submitBtn");
+
+
+let currentCity;
+let savedInput = "";
+let locationApiURL = "";
+let latitude = "";
+let longitude = "";
+
+submitBtn.addEventListener("click", function(){
+    localWeatherCoords(searchInput);
+})
+
+function urlCall(url){
+    fetch(url).then(
+        response => response.json()
+    ).then(
+        data => {
+            currentCity = data;
+        }
+    );
+}
+
+function localWeatherCoords(searchInput){
+    savedInput = searchInput.value;
+    locationApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + savedInput + "&limit=1&appid=543bd604775c751180b1c9722a1db386";
+    urlCall(locationApiURL);
+    // latitude = currentCity.lat;
+    // longitude = currentCity.lon;
+    console.log(currentCity);
+}
